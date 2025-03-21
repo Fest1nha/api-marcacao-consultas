afce7f9d253637387edefe0807ba22c0ece11177
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 20:34:00 -0300

    Atualização da API

--- src/main/java/com/fiap/ecr/api_marcacao_consultas/repository/ConsultaRepository.java
+++ src/main/java/com/fiap/ecr/api_marcacao_consultas/repository/ConsultaRepository.java
@@ -2,7 +2,6 @@ package com.fiap.ecr.api_marcacao_consultas.repository;
 import com.fiap.ecr.api_marcacao_consultas.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-public interface ConsultaRepository extends JpaRepository<Consulta, Long>
-{
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     List<Consulta> findByUsuarioId(Long usuarioId);
 }
--- src/main/java/com/fiap/ecr/api_marcacao_consultas/repository/EspecialidadeRepository.java
+++ src/main/java/com/fiap/ecr/api_marcacao_consultas/repository/EspecialidadeRepository.java
@@ -1,6 +1,5 @@
 package com.fiap.ecr.api_marcacao_consultas.repository;
 import com.fiap.ecr.api_marcacao_consultas.model.Especialidade;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long>
-{
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
 }
